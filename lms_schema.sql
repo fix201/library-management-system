99bff2c10e853ad8fc32329fe37ab541711fda96
zabdoulbachir@gmail.com <zabdoulbachir@gmail.com>
2023-01-25 22:07:19 -0600

    "utf8mb4" added

--- lms_schema.sql
+++ lms_schema.sql
@@ -10,7 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema lms
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lms` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `lms` DEFAULT CHARACTER SET utf8mb4 ;
 USE `lms` ;
 
 -- -----------------------------------------------------
