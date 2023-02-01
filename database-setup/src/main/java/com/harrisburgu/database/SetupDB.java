7e255860ea3ee9b14aae000deedbcb2119bf0c88
Aung Kyaw Min <48268937+AungKyaw-Min@users.noreply.github.com>
2023-02-01 00:13:42 -0600

    Running script and inserting data

--- database-setup/src/main/java/com/harrisburgu/database/SetupDB.java
+++ database-setup/src/main/java/com/harrisburgu/database/SetupDB.java
@@ -1,48 +1,46 @@
 package com.harrisburgu.database;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SetupDB {
 	
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
     static final String DB_URL = "jdbc:mariadb://localhost:3306/lms";
-    
+
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "";
+    static final String PASS = "password";
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException {
 
         Logger logger = LoggerFactory.getLogger(SetupDB.class);
 
         //STEP 2: Register JDBC driver
-		
-		System.out.println("Hello World");
 		Class.forName(JDBC_DRIVER);
 		
 		//STEP 3: Open a connection
 		System.out.println("Connecting to a selected database...");
 		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		System.out.println("Connected database successfully...");
-		
-        Statement stmt =  conn.createStatement();
-        
-        String sqlStatement = "select* from genre";
-        
-        System.out.println("Selecting all the books");
-        ResultSet rs = stmt.executeQuery(sqlStatement);
-        
-        while(rs.next()) {
-        	System.out.println(rs.getString(1)+" "+rs.getString(2)); 
-        }
+
+        // Reading & Executing the sql files
+        ScriptRunner sr = new ScriptRunner(conn);
+        Reader reader = new BufferedReader(new FileReader("src/main/resources/lms_schema.sql"));
+        sr.runScript(reader);
+
+        reader = new BufferedReader(new FileReader("src/main/resources/data.sql"));
+        sr.runScript(reader);
 	}
 
 }
